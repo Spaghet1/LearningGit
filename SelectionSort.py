911ac32bd57374a3dea850981d7ba1568715c563
Ian Ramin <iramin@ucsd.edu>
2025-09-07 15:58:49 -0700

    Adding Selection Sort

---
+++ SelectionSort.py
@@ -0,0 +1,16 @@
+def selection_sort(arr):
+    l = len(arr)
+    for i in range(l - 1):
+        minIndex = i
+        min = arr[i]
+        for j in range(i+1, l):
+            if arr[j] < min:
+                min = arr[j]
+                minIndex = j
+        temp = arr[i]
+        arr[i] = min
+        arr[minIndex] = temp
+
+arr = [4,2,3,4]
+selection_sort(arr)
+print(arr)
